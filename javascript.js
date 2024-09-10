d5d53028e83c271bece004b6c350bab400359d45
Arianda Baidan <arianda.baidan@gmail.com>
2024-09-10 10:46:49 +0300

    Trecia ikelimas

---
+++ javascript.js
@@ -0,0 +1,11 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
+
+function showSlide(index) {
+  slides[currentSlide].classList.remove("active");
+  dots[currentSlide].classList.remove("active");
+  currentSlide = index;
+  slides[currentSlide].classList.add("active");
+  dots[currentSlide].classList.add("active");
+}
